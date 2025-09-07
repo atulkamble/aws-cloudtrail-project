b517dd4865d120c302f6f6c5f775d9f420bfa65b
Atul Kamble <atul_kamble@hotmail.com>
2025-09-07 14:57:42 +0530

    Update cloudwatch.tf

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,4 +1,5 @@
 resource "aws_cloudwatch_log_group" "cloudtrail" {
-  name              = "/aws/cloudtrail/${var.trail_name}"
-  retention_in_days = 90
+name = "/aws/cloudtrail/${var.trail_name}"
+retention_in_days = var.log_retention_days
+tags = local.common_tags
 }

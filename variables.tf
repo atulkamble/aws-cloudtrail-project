f394f7db3750e08ebf024567fe7c2d01e2cdff7f
Atul Kamble <atul_kamble@hotmail.com>
2025-09-07 14:56:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,33 @@
 variable "region" {
-  default = "us-east-1"
+description = "AWS region"
+type = string
+default = "us-east-1"
 }
+
 
 variable "trail_name" {
-  default = "cloudnautic-cloudtrail"
+description = "CloudTrail name"
+type = string
+default = "cloudnautic-cloudtrail"
 }
 
+
 variable "bucket_name" {
-  default = "cloudnautic-cloudtrail-logs"
+description = "S3 bucket for CloudTrail logs (must be globally unique)"
+type = string
+default = "cloudnautic-cloudtrail-logs-123456"
+}
+
+
+variable "log_retention_days" {
+description = "CloudWatch Logs retention"
+type = number
+default = 90
+}
+
+
+variable "enable_athena" {
+description = "Create Athena/Glue resources"
+type = bool
+default = false
 }

64f9d1352205e883c36ebb0c99ad03f6d45b1627
Atul Kamble <atul_kamble@hotmail.com>
2025-09-07 14:57:26 +0530

    Update kms.tf

--- kms.tf
+++ kms.tf
@@ -1,10 +1,12 @@
 resource "aws_kms_key" "cloudtrail" {
-  description             = "KMS key for CloudTrail logs"
-  deletion_window_in_days = 7
-  enable_key_rotation     = true
+description = "KMS key for CloudTrail logs"
+enable_key_rotation = true
+deletion_window_in_days = 7
+tags = local.common_tags
 }
 
+
 resource "aws_kms_alias" "cloudtrail" {
-  name          = "alias/cloudtrail-key"
-  target_key_id = aws_kms_key.cloudtrail.key_id
+name = "alias/cloudtrail-key"
+target_key_id = aws_kms_key.cloudtrail.key_id
 }
